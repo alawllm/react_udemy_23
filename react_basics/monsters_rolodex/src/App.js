09b85fdc2fc82b6195fa6521b64b35a17cc2a956
alawllm <alicja.willam@gmail.com>
2023-05-02 13:16:03 +0200

    state and class basics

--- react_basics/monsters_rolodex/src/App.js
+++ react_basics/monsters_rolodex/src/App.js
@@ -1,25 +1,33 @@
+import { Component } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  //constructor method will run before anything else
+  constructor() {
+    super();
+    //initializing state 
+    //state - always json object
+    this.state = {
+      name: 'Ala'
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello I am {this.state.name}
+          </p>
+          <button>Change name</button>
+        </header>
+      </div>
+    )
+  }
 }
+
 
 export default App;
