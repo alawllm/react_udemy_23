9d998a14632ce97a685ea29d3ce9d471ce4a6dd6
alawllm <alicja.willam@gmail.com>
2023-06-27 16:16:32 +0200

    category preview, nested routes

--- capstone_project/src/components/category-preview/category-preview.component.jsx
+++ capstone_project/src/components/category-preview/category-preview.component.jsx
@@ -1,11 +1,23 @@
 import './category-preview.styles.scss'
+import ProductCard from '../product-card/product-card.component';
+import { Link } from 'react-router-dom';
 
 const CategoryPreview = ({ title, products }) => {
     return (
         <div className='category-preview-container'>
             <h2>
-                <span>{title.toUpperCase()}</span>
+                <Link className='title' to={title}>{title.toUpperCase()}</Link>
             </h2>
+            <div className='preview'>
+                {
+                    //idx - index number inside of the array
+                    products
+                        .filter((_, idx) => idx < 4)
+                        .map((product) =>
+                            <ProductCard key={product.id} product={product} />
+                        )
+                }
+            </div>
         </div>
     )
 }
--- capstone_project/src/index.js
+++ capstone_project/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
-
 import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
